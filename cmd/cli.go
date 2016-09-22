6c179fe33e10114fb9b80f4be102f7b3ad88d2e2
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-09-22 22:01:22 +0900

    1.0.1

--- cmd/cli.go
+++ cmd/cli.go
@@ -24,7 +24,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = filepath.Base(os.Args[0])
 	app.Usage = "migrate inter mongo database"
-	app.Version = "1.0.0"
+	app.Version = "1.0.1"
 	app.Author = "Yuki Furuta"
 	app.Email = "furushchev@jsk.imi.i.u-tokyo.ac.jp"
 
